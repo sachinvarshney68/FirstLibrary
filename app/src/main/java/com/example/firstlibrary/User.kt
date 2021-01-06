2868aeb4991debafff050d09022832c9a6c58da5
Sachin Varshney <sachin.varshney@successive.tech>
2021-01-06 18:20:24 +0530

    First commit

---
+++ app/src/main/java/com/example/firstlibrary/User.kt
@@ -0,0 +1,12 @@
+package com.example.firstlibrary
+
+class User {
+
+    fun getName():String {
+        return "Sachin Varshney"
+    }
+
+    fun getDesignation():String {
+        return "Sr Software Engineer"
+    }
+}
